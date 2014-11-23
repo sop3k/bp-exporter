46e625e2d47c19f5755ef697ca6d8c37997d33c4
Tomek Sobkowiak <soop3k@gmail.com>
2014-11-23 09:34:15 +0100

    Installer script as argument to makeinstall

--- BPExporter/makeinstall.cmd
+++ BPExporter/makeinstall.cmd
@@ -3,7 +3,7 @@ FOR /F "tokens=*" %%a in ('C:\Git\bin\git.exe status --short') do SET OUTPUT=%%a
 FOR /F "tokens=*" %%a in ('C:\Git\bin\git.exe rev-parse --short HEAD') do SET REV=%%a
 if "%OUTPUT%" == "" (goto ok) else (goto fail)
 :ok
-"C:\Program Files (x86)\Inno Setup 5\iscc.exe" ..\Installer\install_x64.iss /dRevision="%REV%"
+"C:\Program Files (x86)\Inno Setup 5\iscc.exe" "%1" /dRevision="%REV%"
 exit 0
 :fail
 echo "GIT DIRTY. PLEASE COMMIT"
