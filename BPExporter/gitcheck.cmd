33b167a4755dacea3242ac85d3433c2ed53e0c97
Tomek Sobkowiak <soop3k@gmail.com>
2014-11-23 08:44:55 +0100

    Using symbolic link to call git.

--- BPExporter/gitcheck.cmd
+++ BPExporter/gitcheck.cmd
@@ -1,5 +1,5 @@
 @echo off
-FOR /F "tokens=*" %%a in ("%ProgramFiles(x86)%\Git\bin\git.exe status --short") do SET OUTPUT=%%a
+FOR /F "tokens=*" %%a in ('C:\Git\bin\git.exe status --short') do SET OUTPUT=%%a
 if "%OUTPUT%" == "" (goto ok) else (goto fail)
 :ok
 exit 0
